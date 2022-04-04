92d4df9586c5c3525ea3d77fd44cd5dc810f0734
kingbloopy <kingbloopy@gmail.com>
2022-04-04 09:03:57 -0700

    routes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -10,5 +11,9 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("testing more"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
